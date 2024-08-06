998145ec6371f06329241a5e7957efd0b4edccd7
mrdarboe <gsdarboe1@gmail.com>
2024-08-06 22:00:32 +0000

    db schema and model test

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +17,57 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let urlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: String
+});
+
+let urlEntry = new mongoose.model('urlEntry', urlSchema)
+
+//Test Entry
+// const createAndSaveEntry = (done) => {
+//   let newURL = new urlEntry({short_url: "test1"})
+
+//   newURL.save(function(err, data){
+//     if (err) return done(err)
+//     done(null, data);
+//   })
+// }
+
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+/**
+ * create database
+ *  
+ * create schema
+ * create model for adding
+ * 
+ * POST submitted URL
+ *  if url exists in db:
+ *    res corresponding shorturl
+ *  else:
+ *    verify url dns.lookup
+ *      if pass
+ *        generate shorturl (logic?)
+ *          add entry to database
+ *            res.json({original_url: , short_url: )
+ * 
+ * GET api/shorturl/:url
+ *  find url in database
+ *    if found:
+ *      const urlToVisit = found.original_url
+ *        redirect to urlToVisit
+ *    else:
+ *      Not Found
+ */
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
